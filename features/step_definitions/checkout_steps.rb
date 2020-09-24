6c76b3a00340e1ae92f995da02ee64d5d696c8a6
marcusdesouza27 <marcusdesouza27@gmail.com>
2020-09-24 00:34:19 -0300

    Checkout - Product No Prescription - Happy Flow - Done

---
+++ features/step_definitions/checkout_steps.rb
@@ -0,0 +1,27 @@
+Then("sees checkout page") do
+    expect(@checkout.label_oderSummary).to have_content('RESUMEN DE COMPRA')
+end
+
+When("user forwards Step one") do
+    @checkout.stepOneSubmit
+end
+
+When("user confirms Step two") do
+    @checkout.deliveryExp
+    @checkout.stepTwoSubmit
+end
+
+When("user sets payment info") do
+    @checkout.selectCard
+    @checkout.stepthreeDNI
+    @checkout.stepThreeSubmit    
+end
+
+When("user confirms place order") do
+    @checkout.stepFourSubmit
+end
+
+Then("user sees order confirmation page") do
+    expect(@confirmation.title).to have_content('HAS REALIZADO CON ÉXITO TU COMPRA')
+    expect(@confirmation.label_order).to have_content('RESUMEN DE COMPRA')
+end
